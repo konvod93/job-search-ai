4dcd13225710f482b1b76e32e67e21946d9b825b
Dev <dev@example.com>
2026-08-13 12:23:43 +0300

    Update README with 23 final job categories

---
+++ drizzle/0012_pink_secret_warriors.sql
@@ -0,0 +1,10 @@
+ALTER TYPE "public"."job_category" ADD VALUE 'logistics' BEFORE 'agriculture';--> statement-breakpoint
+ALTER TYPE "public"."job_category" ADD VALUE 'medical' BEFORE 'other';--> statement-breakpoint
+ALTER TYPE "public"."job_category" ADD VALUE 'hospitality' BEFORE 'other';--> statement-breakpoint
+ALTER TYPE "public"."job_category" ADD VALUE 'culture' BEFORE 'other';--> statement-breakpoint
+ALTER TYPE "public"."job_category" ADD VALUE 'science' BEFORE 'other';--> statement-breakpoint
+ALTER TYPE "public"."job_category" ADD VALUE 'facilities_management' BEFORE 'other';--> statement-breakpoint
+ALTER TYPE "public"."job_category" ADD VALUE 'show_business' BEFORE 'other';--> statement-breakpoint
+ALTER TYPE "public"."job_category" ADD VALUE 'media' BEFORE 'other';--> statement-breakpoint
+ALTER TYPE "public"."job_category" ADD VALUE 'service_staff' BEFORE 'other';--> statement-breakpoint
+ALTER TYPE "public"."job_category" ADD VALUE 'utilities' BEFORE 'other';
---
+++ drizzle/0013_burly_titania.sql
@@ -0,0 +1,2 @@
+ALTER TYPE "public"."job_category" ADD VALUE 'legal' BEFORE 'other';--> statement-breakpoint
+ALTER TYPE "public"."job_category" ADD VALUE 'management_marketing' BEFORE 'other';
