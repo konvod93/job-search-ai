169265c4b4eb1e7d9d3c87e11b027744616fab63
Dev <dev@example.com>
2026-08-16 17:22:09 +0300

    Add 6 more categories to prevent profeessional-dignitu UX problems: catering (vs hospitality), auto_service (vs manufacturing), maintenance (vs manufacturing), passanger/railway/maritime transport (vs generic drivers)

---
+++ drizzle/0015_dark_legion.sql
@@ -0,0 +1,6 @@
+ALTER TYPE "public"."job_category" ADD VALUE 'catering' BEFORE 'culture';--> statement-breakpoint
+ALTER TYPE "public"."job_category" ADD VALUE 'auto_service' BEFORE 'culture';--> statement-breakpoint
+ALTER TYPE "public"."job_category" ADD VALUE 'maintenance' BEFORE 'culture';--> statement-breakpoint
+ALTER TYPE "public"."job_category" ADD VALUE 'passenger_transport' BEFORE 'culture';--> statement-breakpoint
+ALTER TYPE "public"."job_category" ADD VALUE 'railway_transport' BEFORE 'culture';--> statement-breakpoint
+ALTER TYPE "public"."job_category" ADD VALUE 'maritime_transport' BEFORE 'culture';
